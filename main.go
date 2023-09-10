3770f7534917456b9c216e5bd15dff080051fa75
muhammetozekli <62158221+muhammetozekli@users.noreply.github.com>
2023-09-10 16:42:44 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "gitlab.trendyol.com/platform/devops/base/apps/sre/sre-core/sre-troubleshooter/cmd"
+import "sre-troubleshooter/cmd"
 
 func main() {
 	cmd.Execute()
